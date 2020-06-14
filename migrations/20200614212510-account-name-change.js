03b03083236bd45a40b10c1134e11fe811268657
omersayem <sayemomer@gmail.com>
2020-06-15 03:49:56 +0600

    first commit

---
+++ migrations/20200614212510-account-name-change.js
@@ -0,0 +1,25 @@
+module.exports = {
+  async up(db, client) {
+    const user = await db.collection('accounts').find({}).toArray();
+    console.log(user)
+    user.forEach(async element => {
+
+      let { firstName , lastName } = element ;
+      const name = firstName + ' ' + lastName + Date.now();
+      await db.collection('accounts').updateOne({_id : element._id},{$set : {'name' : name}})
+    });
+  },
+
+  async down(db, client) {
+    const user = await db.collection('accounts').find({}).toArray();
+    console.log(user)
+    let i = 1 ;
+    user.forEach(async element => {
+
+      let { firstName , lastName } = element ;
+      const name = firstName + '-' + lastName + i;
+      await db.collection('accounts').updateOne({_id : element._id},{$set : {'name' : name}})
+      i++;
+    });
+  }
+};
